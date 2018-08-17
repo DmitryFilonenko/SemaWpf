c93895f716e87f8dd5c0f54bf9fb99a60369755f
Dmitry Filonenko <feelll@bigmir.net>
2018-08-17 18:03:33 +0300

    InsertToDb, DeleteFromDb have done.

---
+++ Sema/Mediator/MediatorSema.cs
@@ -0,0 +1,15 @@
+﻿using Sema.DbLayer.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sema.Mediator
+{
+    static class MediatorSema
+    {
+        public static TableState UsingTable { get; set; }
+        public static string TableName { get; set; }
+    }
+}
